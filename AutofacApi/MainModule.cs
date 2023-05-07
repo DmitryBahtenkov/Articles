6c4e702182a1b1cb69fd26e64a355a146affb985
Iridium <dmitrybahtenkov@outlook.com>
2023-05-07 14:31:44 +0300

    я

---
+++ AutofacApi/MainModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+
+namespace AutofacApi;
+
+public class MainModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        base.Load(builder);
+    }
+}
---
+++ AutofacApi/Program.cs
@@ -0,0 +1,32 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>((containerBuilder) =>
+{
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void ConfigureContainer(ContainerBuilder container)
+{
+}
